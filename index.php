571f78c95302655d1c2bb83d9f43b8dc23c2ea20
wnykhza <fathorrosi1777@gmail.com>
2021-05-12 11:02:59 +0700

    add session and cookie

--- index.php
+++ index.php
@@ -1,3 +1,78 @@
+<?php 
+session_start();
+
+$databaseHost = 'localhost';
+$databaseName = 'account';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+$err        = "";
+$username   = "";
+$rememberMe   = "";
+
+if(isset($_COOKIE['cookie_username'])){
+    $cookie_username = $_COOKIE['cookie_username'];
+    $cookie_password = $_COOKIE['cookie_password'];
+
+    $sql1 = "select * from account where username = '$cookie_username'";
+    $q1   = mysqli_query($mysqli, $sql1);
+    $r1   = mysqli_fetch_array($q1);
+    if($r1['password'] == $cookie_password){
+        $_SESSION['session_username'] = $cookie_username;
+        $_SESSION['session_password'] = $cookie_password;
+    }
+}
+
+if(isset($_SESSION['session_username'])){
+    header("location:member.php");
+    exit();
+}
+
+if(isset($_POST['login'])){
+    $username   = $_POST['username'];
+    $password   = $_POST['password'];
+    $rememberMe   = $_POST['rememberMe'];
+
+    if($username == '' or $password == ''){
+        $err .= "<li>Please enter your username and password</li>";
+    }else{
+        $sql1 = "select * from account where username = '$username'";
+        $q1   = mysqli_query($mysqli, $sql1);
+        $r1   = mysqli_fetch_array($q1);
+
+        if($r1['username'] == ''){
+            $err .= "<li>Username <b>$username</b> not found.</li>";
+        }elseif($r1['password'] != md5($password)){
+            $err .= "<li>Incorrect password entered</li>";
+        }       
+        
+        if(empty($err)){
+            $_SESSION['session_username'] = $username; 
+            $_SESSION['session_password'] = md5($password);
+
+            if($rememberMe == 1){
+                $cookie_name = "cookie_username";
+                $cookie_value = $username;
+                $cookie_time = time() + (60 * 60 * 24 * 30);
+                setcookie($cookie_name,$cookie_value,$cookie_time,"/");
+
+                $cookie_name = "cookie_password";
+                $cookie_value = md5($password);
+                $cookie_time = time() + (60 * 60 * 24 * 30);
+                setcookie($cookie_name,$cookie_value,$cookie_time,"/");
+            }
+            header("location:member.php");
+        }
+    }
+}
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,24 +96,22 @@
                         <div class="row">
                             <div class="col-md-9 col-lg-8 mx-auto">
                                 <h3 class="login-heading mb-4">Welcome back!</h3>
-                                <form>
+                                <form id="loginform" class="form-horizontal" action="" method="post" role="form">
                                     <div class="form-label-group">
-                                        <input type="username" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
+                                        <input type="username" id="username" class="form-control" placeholder="Username" name="username" required autofocus>
                                         <label for="inputEmail">Username</label>
                                     </div>
 
                                     <div class="form-label-group">
-                                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                                        <input type="password" id="password" class="form-control" placeholder="Password" name="password" required autofocus>
                                         <label for="inputPassword">Password</label>
                                     </div>
 
                                     <div class="custom-control custom-checkbox mb-3">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="custom-control-label" for="customCheck1">Remember password</label>
+                                        <input id="login-remember" type="checkbox" name="rememberMe" value="1" <?php if($rememberMe == '1') echo "checked"?>>
+                                        <label class="custom-control-label" for="customCheck1">Remember Me</label>
                                     </div>
-                                    <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">Log In</button>
-                                    <div class="text-center">
-                                        <a class="small" href="#">Forgot password?</a></div>
+                                    <input class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit" value="Log In" name="submit">
                                 </form>
                             </div>
                         </div>
